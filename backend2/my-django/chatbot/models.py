3ffd0b6a89647853b1b82a571ebc62efab907975
unknown <lionx3@naver.com>
2021-11-23 18:17:08 +0900

    1123

--- backend2/my-django/chatbot/models.py
+++ backend2/my-django/chatbot/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import pandas as pd
+import urllib.request
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/songys/Chatbot_data/master/ChatbotData.csv", filename="ChatBotData.csv")
+
+train_data = pd.read_csv('ChatBotData.csv')
+train_data.head()
 
 # Create your models here.
+class Chatbot(models.Model):
+    def __init__(self):
+        pass
+
+
+    class Meta:
+        db_table = "chatbot"
